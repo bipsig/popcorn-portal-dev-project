103bf25c7d99567b11ef6861d19d7004ae936af2
bipsig <sagnikd.ug20.ece@nitp.ac.in>
2023-07-04 02:07:51 +0530

    Created the footer component

--- src/components/footer/Footer.jsx
+++ src/components/footer/Footer.jsx
@@ -1,13 +1,58 @@
-import React from 'react'
+import React from "react";
+import {
+    FaFacebookF,
+    FaInstagram,
+    FaTwitter,
+    FaLinkedin,
+} from "react-icons/fa";
+
+import ContentWrapper from "../contentWrapper/ContentWrapper";
 
 import "./style.scss";
 
 const Footer = () => {
-  return (
-    <div>
-        Footer
-    </div>
-  )
-}
+    return (
+        <footer className="footer">
+            <ContentWrapper>
+                <ul className="menu-items">
+                    <li className="menu-item">Terms Of Use</li>
+                    <li className="menu-item">Privacy-Policy</li>
+                    <li className="menu-item">About</li>
+                    <li className="menu-item">Blog</li>
+                    <li className="menu-item">FAQ</li>
+                </ul>
 
-export default Footer
+                <div className="info-text">
+                    Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed
+                    do eiusmod tempor incididunt ut labore et dolore magna
+                    aliqua. Ut enim ad minim veniam, quis nostrud exercitation
+                    ullamco laboris nisi ut aliquip ex ea commodo consequat.
+                    Duis aute irure dolor in reprehenderit in voluptate velit
+                    esse cillum dolore eu fugiat nulla pariatur.
+                </div>
+
+                <div className="social-icons">
+
+                    <span className="icon">
+                        <FaFacebookF />
+                    </span>
+
+                    <span className="icon">
+                        <FaInstagram />
+                    </span>
+
+                    <span className="icon">
+                        <FaTwitter />
+                    </span>
+
+                    <span className="icon">
+                        <FaLinkedin />
+                    </span>
+					
+                </div>
+            </ContentWrapper>
+        </footer>
+    );
+};
+
+export default Footer;
