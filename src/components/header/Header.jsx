48cf036b79ebb078c2d1293c60f735172267134a
bipsig <sagnikd.ug20.ece@nitp.ac.in>
2023-07-03 23:17:46 +0530

    Initial files aded for the project

--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -6,17 +6,7 @@ import { useNavigate, useLocation } from "react-router-dom";
 
 import "./style.scss";
 
-import ContentWrapper from "../contentWrapper/ContentWrapper";
-import logo from "../../assets/movix-logo.svg";
-
 const Header = () => {
-    const [show, setShow] = useState("top");
-    const [lastScrollY, setLastScrollY] = useState(0);
-    const [mobileMenu, setMobileMenu] = useState(false);
-    const [query, setQuery] = useState("");
-    const [showSearch, setShowSearch] = useState("");
-    const navigate = useNavigate();
-    const location = useLocation();
 
     return (
         <div>Header</div>
